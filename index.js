63e21aeccfdea6f74895860f9f111af4d9aa527c
defifofum <defifofum0@gmail.com>
2023-10-31 18:35:42 -0500

    refactor: Artifact export

---
+++ index.js
@@ -0,0 +1,2 @@
+const IERC5725_Artifact = require('./artifacts/contracts/IERC5725.sol/IERC5725.json');
+module.exports = { IERC5725_Artifact };
--- prepare.js
+++ prepare.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 // Define source and destination paths
 const paths = [
+  // Bringing to root for cleaner imports
   { src: './contracts/IERC5725.sol', dest: './IERC5725.sol' },
-  { src: './artifacts/contracts/IERC5725.sol/IERC5725.json', dest: './IERC5725.json' },
 ];
 
+console.log("Preparing for publish...");
 // Copy each file
 paths.forEach(({ src, dest }) => {
   fs.copyFileSync(src, dest);
 });
+
+console.log("Preparing DONE!");
